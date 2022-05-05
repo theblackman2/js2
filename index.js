693673e035c30cdff29c480105a8824b9907fc65
theblackman2 <paskasonga@gmail.com>
2022-05-05 16:22:34 +0100

    Résolu: matriceCarre

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function valMinMax(arr){}
-function matriceCaree(arr){}
+function matriceCaree(arr){
+
+    let sum1 = 0, sum2 = 0
+    for(let i = 0; i < arr.length; i++){
+        sum1 += arr[i][i]
+        sum2 += arr[i][arr[i].length - (i+1)]
+    }
+
+    return Math.abs(sum1 - sum2)
+}
 function camelCase(str){}
 function arcadeRank(top_scores, jeu){}
 
+
 module.exports = { valMinMax, matriceCaree, camelCase, arcadeRank };
+
+
