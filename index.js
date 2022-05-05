0bb9db89b50f31696f62a2038fddc1886f82be44
theblackman2 <paskasonga@gmail.com>
2022-05-05 16:32:21 +0100

    Résolu: camelCase

--- index.js
+++ index.js
@@ -9,9 +9,19 @@ function matriceCaree(arr){
 
     return Math.abs(sum1 - sum2)
 }
-function camelCase(str){}
-function arcadeRank(top_scores, jeu){}
+function camelCase(str){
 
+    let count = 1
+    for(let i = 0; i < str.length; i++){
+        if(str[i] == str[i].toUpperCase()){
+            count += 1
+        }
+    }
+
+    return count
+
+}
+function arcadeRank(top_scores, jeu){}
 
 module.exports = { valMinMax, matriceCaree, camelCase, arcadeRank };
 
