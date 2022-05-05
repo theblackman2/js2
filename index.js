560bf23b5b7411cd1256022a2bed27f5bf5fa306
theblackman <paskasonga@gmail.com>
2022-05-05 14:49:26 +0000

    Initialisation

---
+++ index.js
@@ -0,0 +1,6 @@
+function valMinMax(arr){}
+function matriceCaree(arr){}
+function camelCase(str){}
+function arcadeRank(top_scores, jeu){}
+
+module.exports = { valMinMax, matriceCaree, camelCase, arcadeRank };
